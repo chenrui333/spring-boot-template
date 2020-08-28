187beb8a30f73552968ada8656b8e756532cd915
Rui Chen <rui@meetup.com>
2020-08-28 19:18:23 -0400

    wip

---
+++ src/main/java/com/github/springboot/config/SecurityConfig.java
@@ -0,0 +1,72 @@
+package com.github.springboot.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.saml.userdetails.SAMLUserDetailsService;
+
+import static org.springframework.security.extensions.saml2.config.SAMLConfigurer.saml;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Value("${onelogin.metadata-path}")
+	private String metadataPath;
+
+	@Value("${onelogin.sp.protocol}")
+	private String spProtocol;
+
+	@Value("${onelogin.sp.host}")
+	private String spHost;
+
+	@Value("${onelogin.sp.path}")
+	private String spBashPath;
+
+	@Value("${onelogin.sp.key-store.file}")
+	private String keyStoreFile;
+
+	@Value("${onelogin.sp.key-store.password}")
+	private String keyStorePassword;
+
+	@Value("${onelogin.sp.key-store.alias}")
+	private String keyStoreAlias;
+
+
+	@Value("${onelogin.sp.protocol}")
+	private String protocol;
+
+	@Autowired
+	private SAMLUserDetailsService samlUserService;
+
+	@Override
+	protected void configure(final HttpSecurity http) throws Exception {
+		//@formatter:off
+		http
+				.csrf().and()
+				.authorizeRequests()
+				.antMatchers("/saml/**").permitAll()
+				.anyRequest().authenticated()
+				.and()
+				.apply(saml())
+				.userDetailsService(samlUserService)
+				.serviceProvider()
+				.protocol(spProtocol)
+				.hostname(spHost)
+				.basePath(spBashPath)
+				.keyStore()
+				.storeFilePath(keyStoreFile)
+				.keyPassword(keyStorePassword)
+				.keyname(keyStoreAlias)
+				.and()
+				.and()
+				.identityProvider()
+				.metadataFilePath(metadataPath)
+				.and()
+				.and();
+		//@formatter:on
+	}
+}
